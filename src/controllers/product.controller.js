dc90954a81f00b7fa355ecff68b026db9267e188
vidit19sharma <vidit19sharma@gmail.com>
2025-09-04 05:38:59 +0530

    add product model

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -1,10 +1,19 @@
 import path from 'path';
+//importing product model
+import ProductModel from '../models/product.model.js';
+
 
 //creating es6 module by class
-
 export default class ProductController{
+
     //creating a middleware for prodcuts
     getProducts(req,res){
+
+    //getting data from models
+    //static method so need to create instamce to call
+    let products = ProductModel.getTestData();
+    console.log(products)
+
         //sending the file back to client
         return res.sendFile(path.join(path.resolve(),'src','views','products.html'))
     }
